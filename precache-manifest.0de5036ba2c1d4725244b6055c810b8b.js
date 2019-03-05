7a36b010e3b3429aa02748346fd14022916868cd
Ian Walston <ikwalston@comcast.net>
2019-03-05 01:32:21 -0500

    animated text for noticability

---
+++ precache-manifest.0de5036ba2c1d4725244b6055c810b8b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9cf7dc1c19a28b16831",
+    "url": "./static/js/main.c9cf7dc1.chunk.js"
+  },
+  {
+    "revision": "4ac4ad7172d3e1668cee",
+    "url": "./static/js/1.4ac4ad71.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "eeb39f7fcd8d6fa774c4396460fdae42",
+    "url": "./index.html"
+  }
+];
